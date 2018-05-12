612c68f149dc15e2ae0f5ddc8fc7c35e655955ba
Lee Green <lee.x.green@gmail.com>
2018-05-11 19:37:33 -0500

    more themes

--- build_all.sh
+++ build_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for t in elegant eloquent flat flat-optimized kendall kwan material modern nominaltech onepage paper sceptile short slick spartan stackoverflow crisp flat modern
+for t in caffeine elegant eloquent flat flat-optimized kendall kwan material modern nominaltech onepage paper sceptile short slick spartan stackoverflow crisp flat modern
 do
   resume export -t $t resume-$t.html
   resume export -t $t resume-$t.pdf
