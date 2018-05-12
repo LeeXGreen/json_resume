f7a0b107da2733bc1765379aa681c8f69400f3e3
Lee Green <lee.x.green@gmail.com>
2018-05-11 20:12:30 -0500

    html/pdf in parallel

--- build_all.sh
+++ build_all.sh
@@ -3,5 +3,9 @@
 for t in caffeine elegant eloquent flat flat-optimized kendall kwan material modern nominaltech onepage paper sceptile short slick spartan stackoverflow crisp flat modern
 do
   resume export -t $t resume-$t.html
+done &
+
+for t in caffeine elegant eloquent flat flat-optimized kendall kwan material modern nominaltech onepage paper sceptile short slick spartan stackoverflow crisp flat modern
+do
   resume export -t $t resume-$t.pdf
-done
+done &
